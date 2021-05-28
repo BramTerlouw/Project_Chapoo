5f168d8aad325b6a626fcf8794a41e98df8e569c
BramTerlouw <70586317+BramTerlouw@users.noreply.github.com>
2021-05-28 13:50:15 +0200

    code Bas bon

--- DAL/Bon_DAO.cs
+++ DAL/Bon_DAO.cs
@@ -1,8 +1,48 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using Model_Chapoo;
 
 namespace DAL_Chapoo
 {
-    public class Bon_DAO
+    public class Bon_DAO : Base
     {
+        public List<Bon> Db_Get_Orders_Per_Table(int TafelID)
+        {
+            string query = "SELECT BonID, BestellingID, TafelID, Totaalbedrag, Fooi, Betaalmethode FROM Bon WHERE TafelID = @TafelID";
+            SqlParameter[] sqlParameters = new SqlParameter[1];
+            sqlParameters[0] = new SqlParameter("@TafelID", TafelID);
+            return ReadTables(ExecuteSelectQuery(query, sqlParameters));
+        }
+
+        public void Db_Update_OrderStatus_Afgerond(int BestellingID)
+        {
+            string query = "UPDATE Bestelling SET Status = 'afgerond' WHERE BestellingID = @OrderID";
+            SqlParameter[] sqlParameters = new SqlParameter[2];
+            sqlParameters[0] = new SqlParameter("@OrderID", BestellingID);
+
+            ExecuteEditQuery(query, sqlParameters);
+        }
+
+        private List<Bon> ReadTables(DataTable dataTable)
+        {
+            List<Bon> bonLijst = new List<Bon>();
+
+            foreach (DataRow dr in dataTable.Rows)
+            {
+                Bon bon = new Bon()
+                {
+                    BonID = (int)dr["BonID"],
+                    BestellingID = (int)dr["BestellingID"],
+                    TafelID = (int)dr["TafelID"],
+                    TotaalBedrag = (double)dr["Totaalbedrag"],
+                    Fooi = (double)dr["Fooi"],
+                    BetaalMethode = (string)dr["Betaalmethode"]
+                };
+                bonLijst.Add(bon);
+            }
+            return bonLijst;
+        }
     }
 }
--- Model/Bon.cs
+++ Model/Bon.cs
@@ -4,5 +4,11 @@ namespace Model_Chapoo
 {
     public class Bon
     {
+        public int BonID { get; set; }
+        public int BestellingID { get; set; }
+        public int TafelID { get; set; }
+        public double TotaalBedrag { get; set; }
+        public double Fooi { get; set; }
+        public string BetaalMethode { get; set; }
     }
 }
