e8e660ce9f8eaae5e8dd7e648ee63e2cdff3dc6a
GEZUS95 <ceesgribnau@hotmail.com>
2021-06-09 15:26:09 +0200

    ophalen van bestellingdetails toegevoegd

--- DAL/BestellingRegel_DAO.cs
+++ DAL/BestellingRegel_DAO.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Model_Chapoo;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
@@ -18,12 +20,48 @@ namespace DAL_Chapoo
             sqlParameters[1] = new SqlParameter("@Regel", ExecuteCount(query, sqlParameters));
             ExecuteEditQuery(query, sqlParameters);
         }
+
+        public List<BestellingRegel> Db_EetBestellingRegelOphalenByID(int ID)
+        {
+            string query = "SELECT BestellingID, RegelNR, MenuItemID, Aantal, Opmerking FROM BestellingRegel WHERE BestellingID = @BestellingID AND MenuItemID IN (SELECT MenuItemID FROM MenuItem WHERE Soort NOT LIKE '%Drank%')";
+            SqlParameter[] sqlParameters = new SqlParameter[1];
+            sqlParameters[0] = new SqlParameter("@BestellingID", ID);
+            return ReadTables(ExecuteSelectQuery(query, sqlParameters));
+        }
+
+        public List<BestellingRegel> Db_DrankBestellingRegelOphalenByID(int ID)
+        {
+            string query = "SELECT BestellingID, RegelNR, MenuItemID, Aantal, Opmerking FROM BestellingRegel WHERE BestellingID = @BestellingID AND MenuItemID IN (SELECT MenuItemID FROM MenuItem WHERE Soort LIKE '%Drank%')";
+            SqlParameter[] sqlParameters = new SqlParameter[1];
+            sqlParameters[0] = new SqlParameter("@BestellingID", ID);
+            return ReadTables(ExecuteSelectQuery(query, sqlParameters));
+        }
+
         public void Db_VerwijderBestelling(int ID)
         {
             string query = "DELETE BestellingID, RegelNR, MenuItemID, Aantal FROM BestellingRegel WHERE BestellingID = @BestellingID";
             SqlParameter[] sqlParameters = new SqlParameter[1];
             sqlParameters[0] = new SqlParameter("@BestellingID", ID);
             ExecuteEditQuery(query, sqlParameters);
+        }
+
+        private List<BestellingRegel> ReadTables(DataTable dataTable)
+        {
+            List<BestellingRegel> bestellingLijst = new List<BestellingRegel>();
+
+            foreach (DataRow dr in dataTable.Rows)
+            {
+                BestellingRegel bestelling = new BestellingRegel()
+                {
+                    BestellingID = (int)dr["BestellingID"],
+                    RegelNR = (int)dr["RegelNR"],
+                    MenuItemID = (int)dr["MenuItemID"],
+                    Opmerking = (string)dr["Opmerking"],
+                    Aantal = (int)dr["Aantal"]
+                };
+                bestellingLijst.Add(bestelling);
+            }
+            return bestellingLijst;
         }
     }
 }
