1c37f56f9641865c0ae467d6c247fc34773fe93c
GEZUS95 <ceesgribnau@hotmail.com>
2021-06-10 18:24:48 +0200

    rsu

--- DAL/BestellingRegel_DAO.cs
+++ DAL/BestellingRegel_DAO.cs
@@ -1,11 +1,7 @@
 ﻿using Model_Chapoo;
-using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DAL_Chapoo
 {
