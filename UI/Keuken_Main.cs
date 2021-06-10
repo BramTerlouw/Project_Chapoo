cb55e570dabd76df950d2a1f76d3ed155da6dd21
GEZUS95 <ceesgribnau@hotmail.com>
2021-06-10 16:23:26 +0200

    rsu

--- UI/Keuken_Main.cs
+++ UI/Keuken_Main.cs
@@ -2,10 +2,7 @@
 using Service_Chapoo;
 using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Text;
 using System.Windows.Forms;
 
 namespace UI
