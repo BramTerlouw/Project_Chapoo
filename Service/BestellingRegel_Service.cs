991921c577028e690626409bfe975094f1f43af9
GEZUS95 <ceesgribnau@hotmail.com>
2021-06-10 20:55:11 +0200

    rsu, methode toegevoegd en _ voor private

--- Service/BestellingRegel_Service.cs
+++ Service/BestellingRegel_Service.cs
@@ -1,25 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using DAL_Chapoo;
+﻿using DAL_Chapoo;
 using Model_Chapoo;
+using System.Collections.Generic;
 
 namespace Service_Chapoo
 {
     public class BestellingRegel_Service
     {
-        BestellingRegel_DAO bestellingRegel_DAO = new BestellingRegel_DAO();
+        private BestellingRegel_DAO _bestellingRegel_DAO = new BestellingRegel_DAO();
 
         public void Db_VoegBestellingToe(int MenuItemID, int Aantal)
         {
-            bestellingRegel_DAO.Db_VoegBestellingToe(MenuItemID, Aantal);
+            _bestellingRegel_DAO.Db_VoegBestellingToe(MenuItemID, Aantal);
         }
 
         public List<BestellingRegel> GetEetBestellingDetailsByBestellingID(int bestellingID)
         {
-            return bestellingRegel_DAO.Db_EetBestellingRegelOphalenByID(bestellingID);
+            return _bestellingRegel_DAO.Db_EetBestellingRegelOphalenByID(bestellingID);
+        }
+
+        public List<BestellingRegel> GetDrinkBestellingDetailsByBestellinID(int bestellingID)
+        {
+            return _bestellingRegel_DAO.Db_DrankBestellingRegelOphalenByID(bestellingID);
         }
     }
 }
